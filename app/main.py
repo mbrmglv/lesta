2fef1af69a1bb62707b7ed72b17d4a942ed2bb83
mbrmglv <mbrmglv@gmail.com>
2025-04-27 15:15:32 +0300

    fix: lint check

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
-from fastapi.staticfiles import StaticFiles
 from fastapi.openapi.utils import get_openapi
 
 from app.api import router
